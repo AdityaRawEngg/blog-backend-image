7cee671ab271012d2af291e2f97dc94584366318
AdityaRawEngg <aditya.shukla@raweng.com>
2020-12-18 15:43:50 +0530

    Created app.js
    connected to mongodb
    server started

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const blogRoute = require("./routes/blogRoute");
+
+dotenv.config({ path: "./config.env" });
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+
+mongoose.connect(
+  process.env.DATABASE_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, connection) => {
+    if (err) {
+      return console.log("Error while connecting");
+    }
+    app.use("/blogs", blogRoute);
+    // app.post("/", (req, resp) => {
+
+    // });
+    app.listen(process.env.PORT, () => {
+      console.log(`Server started on Port ${PORT}`);
+    });
+  }
+);
